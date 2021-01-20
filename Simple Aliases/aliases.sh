5ae9eefcb4bf181014e39f3fee9a09c0b8d8a9ef
肉球の星遅れ <69009801+pawstar-lag@users.noreply.github.com>
2021-01-20 11:33:02 -0800

    Added alias ali

--- Simple Aliases/aliases.sh
+++ Simple Aliases/aliases.sh
@@ -2,3 +2,9 @@
 alias path = "echo $PATH"
 alias pi = "echo 'scale=1000; 4*a(1)|bc -l'"
 alias sl = "ls -aLf"
+alias ali = "alias"
+ali ld = "ls -a"
+# Git needs to be installed.
+ali push = "git push"
+ali pull = "git pull"
+ali addall = "git add ."

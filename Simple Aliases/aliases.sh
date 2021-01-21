702f3701f208138a3b926bdc210258ae760c2285
pawstar-lag <toren.gulati@gmail.com>
2021-01-21 13:50:40 -0800

    Another alias

--- Simple Aliases/aliases.sh
+++ Simple Aliases/aliases.sh
@@ -1,6 +1,6 @@
 # Basic environment variables are already defined.
 alias path = "echo $PATH"
-alias pi = "echo 'scale=1000; 4*a(1)|bc -l'"
+alias pi = "echo 'scale=1000; 4*a(1)'|bc -l"
 alias sl = "ls -aLf"
 alias ali = "alias"
 alias ld = "ls -a"
@@ -8,3 +8,4 @@ alias ld = "ls -a"
 alias push = "git push"
 alias pull = "git pull"
 alias addall = "git add ."
+alias expomarker = "export"
